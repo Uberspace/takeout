959da2d7b7424c5e75f97f490805d9a0ee76db6e
luto <m@luto.at>
2019-06-26 15:38:51 +0200

    refactor UberspaceVersionMixin to give Items access to uberspace version

--- uberspace_takeout/items/base.py
+++ uberspace_takeout/items/base.py
@@ -45,10 +45,13 @@ class PathItem(TakeoutItem):
 class UberspaceVersionMixin():
     uberspace_version = None
 
-    def is_active(self):
+    @property
+    def current_uberspace_version(self):
         with open('/etc/centos-release') as f:
             text = f.read()
             # looks like "CentOS release 6.10 (Final)"
             centos_release = re.search(r'release ([0-9])+\.', text).groups()[0]
+            return centos_release
 
-        return int(centos_release) == int(self.uberspace_version)
+    def is_active(self):
+        return self.current_uberspace_version == int(self.uberspace_version)

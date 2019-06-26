730b59fe4db708cd76ee363746747c4d736e608d
luto <m@luto.at>
2019-06-26 14:57:51 +0200

    support writing tarballs to STDOUT, close #1

--- uberspace_takeout/__main__.py
+++ uberspace_takeout/__main__.py
@@ -8,17 +8,22 @@ from . import TakeoutU7
 def main():
     username = getpass.getuser()
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
-    tar_path = 'takeout_{}_{}.tar.bz2'.format(username, timestamp)
+    default_tar_path = 'takeout_{}_{}.tar.bz2'.format(username, timestamp)
 
     p = argparse.ArgumentParser()
     p.add_argument('action', choices=['takeout', 'takein'])
-    p.add_argument('--tar-file', default=tar_path)
+    p.add_argument('--tar-file', default=default_tar_path)
     args = p.parse_args()
 
+    tar_path = args.tar_file
+
+    if tar_path == '-':
+        tar_path = '/dev/stdout'
+
     if args.action == 'takeout':
-        TakeoutU7().takeout(args.tar_file, username)
+        TakeoutU7().takeout(tar_path, username)
     elif args.action == 'takein':
-        TakeoutU7().takein(args.tar_file, username)
+        TakeoutU7().takein(tar_path, username)
     else:
         raise NotImplementedError()
 

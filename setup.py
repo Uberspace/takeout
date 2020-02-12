1f7ad14805183626fae70a760056cc5c643abc25
Janto <janto@team.uber.space>
2020-02-12 13:50:42 +0100

    Revert ":package: add extras for Python 2.7"
    This reverts commit 7aee00b103487c19e60777407682792ed71e7614.

--- setup.py
+++ setup.py
@@ -17,10 +17,7 @@ setup(
     author_email='hallo@uberspace.de',
     url='https://github.com/uberspace/takeout',
     packages=['uberspace_takeout', 'uberspace_takeout.items',],
-    extras_require={
-        'test': ['pyfakefs>=3.6', 'pytest-mock',],
-        'py27': ['configparser', 'pathlib2',],
-    },
+    extras_require={'test': ['pyfakefs>=3.6', 'pytest-mock',],},
     entry_points={
         'console_scripts': ['uberspace-takeout=uberspace_takeout.__main__:main'],
     },
